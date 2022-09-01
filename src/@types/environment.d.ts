d0258ea9dada7a15765aa27407f26823ad01cb45
will-souza97 <rzy.x97@gmail.com>
2022-08-31 23:10:43 -0300

    chore: add deps and config

---
+++ src/@types/environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      FIREBASE_API_KEY: string;
+      FIREBASE_AUTH_DOMAIN: string;
+      FIREBASE_PROJECT_ID: string;
+      FIREBASE_STORAGE: string;
+      FIREBASE_MESSAGING_SENDER_ID: string;
+      FIREBASE_ID: string;
+      FIREBASE_MEASUREMENT_ID: string;
+      PORT: number;
+    }
+  }
+}
+export {};
