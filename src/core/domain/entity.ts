d7b28629bf19d3319af136ccd5da78c907e11c3b
will-souza97 <rzy.x97@gmail.com>
2022-09-20 00:43:54 -0300

    refactor: add created_at and updated_at

--- src/core/domain/entity.ts
+++ src/core/domain/entity.ts
@@ -1,15 +1,22 @@
 import { v4 as uuid } from 'uuid';
 
+type Dates = {
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
 export abstract class Entity<T> {
   protected readonly _id: string;
-  protected readonly props: T;
+  protected readonly props: T & Dates;
 
   get id() {
     return this._id;
   }
 
-  constructor(props: T, id?: string) {
+  constructor(props: T & Dates, id?: string) {
     this._id = id ?? uuid();
+    this.props.createdAt = props.createdAt ?? new Date();
+    this.props.updatedAt = new Date();
     this.props = props;
   }
 }
